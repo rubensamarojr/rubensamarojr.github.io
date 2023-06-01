ddfe0459220eee304c718cc7f1db7d3aaad0a118
rubensamarojr <rubens.amaro@usp.br>
2023-05-31 22:51:10 -0300

    Add talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,42 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Ouro Preto, Brazil | The XXXII CILAMCE | <a href='https://rubensamarojr.github.io/talks/talks/2011-11-13-cilamce'>Estudo numérico da dinâmica de sólidos elásticos por um método de partículas</a>",
+    -20.384498462,
+    -43.502164658
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Pirenópolis, Brazil | The 10th WCCM | <a href='https://rubensamarojr.github.io/talks/talks/2012-07-08-wccm'>Deformation and fracture analysis of elastic solids based on a particle method</a>",
+    -15.8538,
+    -48.9586
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "São Paulo, Brazil | The XXXIV CILAMCE | <a href='https://rubensamarojr.github.io/talks/talks/2013-11-10-cilamce'>Método de partículas para problemas hidroelásticos com superfície livre</a>",
+    -23.533773,
+    -46.625290
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Buenos Aires, Argentina | The 1st PANACM | <a href='https://rubensamarojr.github.io/talks/talks/2015-04-27-panacm'>The influence of geometrical parameters on the performance of perforated breakwater</a>",
+    -34.6083,
+    -58.3712
+  ],
+  [
+    "Brasília, Brazil | The XXXVII CILAMCE | <a href='https://rubensamarojr.github.io/talks/talks/2016-11-06-cilamce'>Estudo numérico da hidrodinâmica de anteparas inclinadas e perfuradas para a proteção do impacto de ondas</a>",
+    -15.793889,
+    -47.882778
+  ],
+  [
+    "Okayama, Japan | JASNAOE 2016 | <a href='https://rubensamarojr.github.io/talks/talks/2016-11-21-jasnaoe'>A numerical study on ore transportation inside curved pipes</a>",
+    34.678631,
+    133.749100
+  ],
+  [
+    "Paraná, Brazil | The 24th COBEM | <a href='https://rubensamarojr.github.io/talks/talks/2017-12-03-cobem'>Hydroelastic effects in structural response under wave impact</a>",
+    -25.441105,
+    -49.276855
+  ],
+  [
+    "Uberlândia, Brazil | The 25th COBEM | <a href='https://rubensamarojr.github.io/talks/talks/2020-09-20-cobem'>Polygon wall boundary model in particle-based method: application to Brumadinho tailings dam failure</a>",
+     -18.913664,
+     -48.266560
   ]
 ];
